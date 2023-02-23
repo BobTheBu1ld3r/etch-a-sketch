41cf60c62df06634d7c2f27f846f71b562140f4e
BobTheBu1ld3r <123880897+BobTheBu1ld3r@users.noreply.github.com>
2023-02-23 19:34:39 +0000

    Write logic to adjust height/width of boxes

--- main.js
+++ main.js
@@ -1,4 +1,10 @@
 const gridContainer = document.querySelector(".grid-container");
+const root = document.querySelector(":root");
+console.log(window.getComputedStyle(root).getPropertyValue("--split"))
+const split = parseFloat(window.getComputedStyle(root).getPropertyValue("--split"));
+const border = parseFloat(window.getComputedStyle(gridContainer).getPropertyValue("border-width"));
+console.log(border);
+console.log(split);
 
 for (let i = 0; i<5 ; i++) {
     const newRow = document.createElement("div");
@@ -7,6 +13,11 @@ for (let i = 0; i<5 ; i++) {
     for (let n = 0; n< 5; n++) {
         const newBox = document.createElement("div");
         newBox.classList.add("box");
+        
+        const computedHeight = (parseFloat(window.getComputedStyle(gridContainer).getPropertyValue("height")) - 4*split -2*border)/5
+        console.log(computedHeight);
+        newBox.style.height = `${computedHeight}px`;
+        newBox.style.width = `${computedHeight}px`
         newRow.appendChild(newBox);
     }
 
